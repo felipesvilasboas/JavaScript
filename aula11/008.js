d4cd92f746751f334ce530956e529f1f486833af
Felipe Vilas Boas <102095497+felipesvilasboas@users.noreply.github.com>
2022-04-04 13:13:45 -0300

    New Exercises

---
+++ aula11/008.js
@@ -0,0 +1,6 @@
+vel = 70.5;
+console.log(`Sua velocidade é ${vel}km/h`);
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Utilize sempre o cinto de segurança!');

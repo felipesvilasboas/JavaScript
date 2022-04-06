5748459a7527006ecd5aab451d544fa61b9dfd59
Felipe Vilas Boas <102095497+felipesvilasboas@users.noreply.github.com>
2022-04-06 09:43:54 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+while (c <= 6) {
+    console.log ('Olá, tudo bem?')
+    c++
+
+}
+
+
+
+
+
+
+
+/*
+console.log ('Olá, tudo bem?')
+console.log ('Olá, tudo bem?')
+console.log ('Olá, tudo bem?')
+console.log ('Olá, tudo bem?')
+console.log ('Olá, tudo bem?')
+console.log ('Olá, tudo bem?')
+*/
